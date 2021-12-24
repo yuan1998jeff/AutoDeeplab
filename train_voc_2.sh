6a087105180f0a64bb0a65b7cc92851830a5d04f
root <jfu.jf60@gmail.com>
2021-12-24 02:32:22 +0000

    added train more layers command

---
+++ train_voc_2.sh
@@ -0,0 +1,1 @@
+python train_autodeeplab.py --backbone resnet --lr 0.007 --workers 4 --epochs 40 --batch_size 1 --gpu_ids 0 --eval_interval 1 --dataset cityscapes --crop_size 256 --num_layers 5 --checkname '5_layers'
